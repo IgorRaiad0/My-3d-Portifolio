5ee8c77225aee9a434752a35a173736482658f11
IgorRaiad0 <igorsonic56@gmail.com>
2026-02-25 00:00:10 -0300

    Removendo modelos não utilizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
 import { BrowserRouter } from "react-router-dom";
 
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 import Preloader from './components/Preloader';
 
-const App =() => {
-  
+const App = () => {
+
   return (
     <>
       <Preloader />
       <BrowserRouter>
-      <div className= "relative z-0 bg-primary">
+        <div className="relative z-0 bg-primary">
 
-        <div className="bg-blue-400">
-          <Navbar/>
-          <Hero/>
+          <div className="bg-blue-400">
+            <Navbar />
+            <Hero />
+          </div>
+
+          <div className="bg-gradient-to-b from-blue-400 via-blue-900 to-primary">
+            <About />
+          </div>
+          <Experience />
+          {/* <Tech/> */}
+          <Works />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+
+
         </div>
-
-        <div className="bg-gradient-to-b from-blue-400 via-blue-900 to-primary">
-          <About/>
-        </div>
-        <Experience/> 
-       {/* <Tech/> */}
-        <Works/>
-        {/* <Feedbacks/> */}
-        <div className="relative z-0">
-          <Contact/>
-          <StarsCanvas/>
-        </div>
-
-
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,7 +1,5 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
 import SpaceManCanvas from "./SpaceMan";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, SpaceManCanvas };
+export { ComputersCanvas, StarsCanvas, SpaceManCanvas };
